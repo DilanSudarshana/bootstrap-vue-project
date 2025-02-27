1e3555b552a4325debfff9f21c1fae0c751ac23c
DilanSudarshana <dilanlakshan18@gmail.com>
2025-02-27 14:32:27 +0530

    add payment form

---
+++ src/components/payment/payment-form.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="col-lg-12">
+    Payment form
+</div>
+
+</template>
+
+<script>
+
+export default {
+    name: 'PaymentForm',
+
+}
+</script>
--- src/components/payment/payment.vue
+++ src/components/payment/payment.vue
@@ -1,10 +1,40 @@
 <template>
-    <h1>Payment</h1>
+    <div class="container">
+
+
+        <div class="row">
+            <Header></Header>
+        </div>
+
+
+        <div class="row">
+            <PaymentForm></PaymentForm>
+        </div>
+
+
+        <div class="row px-5">
+            <PlantItem></PlantItem>
+        </div>
+
+
+    </div>
+    <Footer></Footer>
+
 </template>
 
 <script>
 
+import Footer from '../includes/Footer.vue';
+import Header from '../includes/Header.vue';
+import PaymentForm from './payment-form.vue';
+
 export default {
     name: 'Payment',
+
+    components: {
+        Header,
+        Footer,
+        PaymentForm,
+    },
 }
 </script>
