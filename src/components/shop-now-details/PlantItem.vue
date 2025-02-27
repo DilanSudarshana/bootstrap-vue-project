309db0b88b50fcf13c595f5c386a410228d595ba
DilanSudarshana <dilanlakshan18@gmail.com>
2025-02-27 14:16:32 +0530

    add payment route and page

--- src/components/shop-now-details/PlantItem.vue
+++ src/components/shop-now-details/PlantItem.vue
@@ -1,94 +1,37 @@
 <template>
-    <div class="col-lg-12 p-3">
-        <h5><i class="bi bi-tree-fill"></i> Plant Category Name</h5>
 
-        <div class="row m-1">
-            <div class="overflow-x-scroll mt-5 d-flex">
-
-                <div class="col-lg-3">
-                    <div class="card mb-5" style="width: 18rem;">
-                        <img class="card-img-top"
-                            src="https://www.marthastewart.com/thmb/Gt3EKxaUS8T1WzoSVAmd4UGSFTo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/low-light-houseplants-snake-plant-hero-getty-1123-bb1e9fd1b2024e879a45c3e6bbd9fcfe.jpg"
-                            alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Title</h5>
-                            <p class="card-text">desc</p>
-                            <a href="#" class="btn btn-primary"><i class="bi bi-cart-plus"></i></a>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="col-lg-3">
-                    <div class="card mb-5" style="width: 18rem;">
-                        <img class="card-img-top"
-                            src="https://www.marthastewart.com/thmb/Gt3EKxaUS8T1WzoSVAmd4UGSFTo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/low-light-houseplants-snake-plant-hero-getty-1123-bb1e9fd1b2024e879a45c3e6bbd9fcfe.jpg"
-                            alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Title</h5>
-                            <p class="card-text">desc</p>
-                            <a href="#" class="btn btn-primary"><i class="bi bi-cart-plus"></i></a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3">
-                    <div class="card mb-5" style="width: 18rem;">
-                        <img class="card-img-top"
-                            src="https://www.marthastewart.com/thmb/Gt3EKxaUS8T1WzoSVAmd4UGSFTo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/low-light-houseplants-snake-plant-hero-getty-1123-bb1e9fd1b2024e879a45c3e6bbd9fcfe.jpg"
-                            alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Title</h5>
-                            <p class="card-text">desc</p>
-                            <a href="#" class="btn btn-primary"><i class="bi bi-cart-plus"></i></a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3">
-                    <div class="card mb-5" style="width: 18rem;">
-                        <img class="card-img-top"
-                            src="https://www.marthastewart.com/thmb/Gt3EKxaUS8T1WzoSVAmd4UGSFTo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/low-light-houseplants-snake-plant-hero-getty-1123-bb1e9fd1b2024e879a45c3e6bbd9fcfe.jpg"
-                            alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Title</h5>
-                            <p class="card-text">desc</p>
-                            <a href="#" class="btn btn-primary"><i class="bi bi-cart-plus"></i></a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3">
-                    <div class="card mb-5" style="width: 18rem;">
-                        <img class="card-img-top"
-                            src="https://www.marthastewart.com/thmb/Gt3EKxaUS8T1WzoSVAmd4UGSFTo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/low-light-houseplants-snake-plant-hero-getty-1123-bb1e9fd1b2024e879a45c3e6bbd9fcfe.jpg"
-                            alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Title</h5>
-                            <p class="card-text">desc</p>
-                            <a href="#" class="btn btn-primary"><i class="bi bi-cart-plus"></i></a>
-                        </div>
-                    </div>
-                </div>
-
-
-
-
+    <div v-for="item in plants" :key="item.id" class="col-lg-3 col-sm-12">
+        <div class="card mb-5" style="width: 18rem;">
+            <img class="card-img-top"
+                src="https://www.marthastewart.com/thmb/Gt3EKxaUS8T1WzoSVAmd4UGSFTo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/low-light-houseplants-snake-plant-hero-getty-1123-bb1e9fd1b2024e879a45c3e6bbd9fcfe.jpg"
+                alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">{{ item.title }}</h5>
+                <p class="card-text">{{ item.desc }}</p>
+                <router-link :to="'/payment/' + item.id"><a href="#" class="btn btn-primary">Buy Now <i
+                            class="bi bi-cart-plus"></i></a></router-link>
             </div>
-
         </div>
-
-        <div class="wrapper">
-
-        </div>
-
     </div>
 
 
 </template>
 
 <script>
+import axios from 'axios';
 
 export default {
     name: 'PlantItem',
+
+    data() {
+        return {
+            plants: [],
+        }
+    },
+    async mounted() {
+        let result = await axios.get('http://localhost:3000/plants');
+        this.plants = result.data;
+    }
+
 }
 </script>
